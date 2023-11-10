82bbd2a445cad33f99a5a35684194479e049fd7e
es7eeban <rojasrojasesteban@gmail.com>
2023-11-10 16:37:45 -0300

    home

--- front/src/views/Home.jsx
+++ front/src/views/Home.jsx
@@ -1,7 +1,17 @@
+import { Link } from "react-router-dom";
 const Home = () => {
   return (
     <>
       <h2 className="text-3xl font-bold text-center text-violet-900">Home</h2>
+      <main className="mt-10 text-center">
+        <section>{/* Carousel de imagenes */}</section>
+
+        <Link to={"/productos"}>
+          <button className="w-2/5 py-5 font-semibold tracking-widest text-white uppercase rounded-md btn bg-violet-500/75 hover:bg-violet-700">
+            Ver Productos
+          </button>
+        </Link>
+      </main>
     </>
   );
 };
